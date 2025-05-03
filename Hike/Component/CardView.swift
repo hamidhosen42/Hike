c449058c3fd7a359815e1c8a26d90d101db627a2
Md.Hamid Hosen <mdhamidhosen8444@gmail.com>
2025-05-03 12:59:35 +0600

    Custom Button Style

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct CardView: View {
+
+    // MARK:- PROPERTIES
+
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+
+    //MARK: - FUNCTIONS
+
+    func randomImage() {
+        print("--- BUTTON WAS PRESSED ---")
+        print("Status: Old Image Number = \(imageNumber)")
+
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+            print("Action: Random Number Generated = \(randomNumber)")
+        } while randomNumber == imageNumber
+
+        imageNumber = randomNumber
+
+        print("Result: New Image Number = \(imageNumber)")
+        print("--- THE END ---")
+        print("\n")
+    }
+
     var body: some View {
         //MARK: - CARD
         ZStack {
             CustomBackgroundView()
 
             VStack {
-                
+
                 Spacer()
 
                 // MARK: - HEADER
@@ -52,8 +76,7 @@ struct CardView: View {
                     .foregroundColor(.customGrayMedium)
                 }  //: HEADER
                 .padding(.horizontal, 30)
-                
-                
+
                 // MARK: - MAIN CONTENT
                 ZStack {
                     Circle()
@@ -69,13 +92,39 @@ struct CardView: View {
                         )
                         .frame(width: 256, height: 256)
 
-                    Image("Images/image-1")
+                    Image("Images/image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.easeOut(duration: 1), value: imageNumber)
                 }
 
                 // MARK: - FOOTER (Future content)
-                Spacer()  // placeholder for your footer content
+
+                Button {
+                    // ACTION: Generate a random number
+
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGreenLight,
+                                    .customGreenMedium,
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .shadow(
+                            color: .black.opacity(0.25), radius: 0.25, x: 1,
+                            y: 2)
+                }
+                .buttonStyle(GradientButton())
+
+                Spacer()
             }
         }  //: CARD
         .frame(width: 320, height: 570)
