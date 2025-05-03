bc97c05ba99f1d358d211bb7f306a135c3f87a8c
Md.Hamid Hosen <mdhamidhosen8444@gmail.com>
2025-05-03 12:08:51 +0600

    Header Component

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -9,38 +9,76 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        
-        //MARK : - CARD
-        
+        //MARK: - CARD
         ZStack {
             CustomBackgroundView()
-            
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [
-                                Color("ColorIndigoMedium"),
-                                Color("ColorSalmonLight"),
-                            ],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    )
-                    .frame(
-                        width: 256,
-                        height: 256
-                    )
+
+            VStack {
                 
-                Image("Images/image-1")
-                    .resizable()
-                    .scaledToFit()
+                Spacer()
+
+                // MARK: - HEADER
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        .customGrayLight,
+                                        .customGrayMedium,
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                            )
+
+                        Spacer()
+
+                        Button {
+                            // ACTION: Show a Sheet
+                            print("The button was pressed.")
+                        } label: {
+                            CustomButtonView()
+                        }
+
+                    }
+
+                    Text(
+                        "Fun and enjoyable outdoor activity for friends and families."
+                    )
+                    .multilineTextAlignment(.leading)
+                    .italic()
+                    .foregroundColor(.customGrayMedium)
+                }  //: HEADER
+                .padding(.horizontal, 30)
+                
+                
+                // MARK: - MAIN CONTENT
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color("ColorIndigoMedium"),
+                                    Color("ColorSalmonLight"),
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256, height: 256)
+
+                    Image("Images/image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+
+                // MARK: - FOOTER (Future content)
+                Spacer()  // placeholder for your footer content
             }
-        } //: CARD
-        .frame(
-            width: 320,
-            height: 650
-        )
+        }  //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
