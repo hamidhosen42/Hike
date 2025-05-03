08cf024730efaef31909d748459764fb7410adc4
Md.Hamid Hosen <mdhamidhosen8444@gmail.com>
2025-05-03 07:37:05 +0600

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Sharful Alam on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HikeTests/HikeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HikeTests.swift
+//  HikeTests
+//
+//  Created by Sharful Alam on 3/5/25.
+//
+
+import Testing
+@testable import Hike
+
+struct HikeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
