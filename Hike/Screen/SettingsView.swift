8b201f018394ee1df81138975511678c07577956
Md.Hamid Hosen <mdhamidhosen8444@gmail.com>
2025-05-09 08:48:59 +0600

    SwiftUI List and Labeled Content

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -8,68 +8,175 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+
+    private let alternateAppIcons: [String] = [
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Map",
+        "AppIcon-Mushroom",
+        "AppIcon-Camera",
+        "AppIcon-Backpack",
+        "AppIcon-Campfire",
+    ]
+
     var body: some View {
         List {
             // MARK: - SECTION: HEADER
 
-            HStack {
+            Section {
+                HStack {
+                    Spacer()
 
-                Spacer()
+                    Image(systemName: "laurel.leading")
+                        .font(.system(size: 80, weight: .black))
 
-                Image(systemName: "laurel.leading")
-                    .font(.system(size: 80, weight: .black))
+                    VStack(spacing: -10) {
+                        Text("Hike")
+                            .font(.system(size: 66, weight: .black))
 
-                VStack(spacing: -10) {
-                    Text("Hike")
-                        .font(.system(size: 66, weight: .black))
+                        Text("Editors' Choice")
+                            .fontWeight(.medium)
+                    }
 
-                    Text("Editors' Choice")
-                        .fontWeight(.medium)
+                    Image(systemName: "laurel.trailing")
+                        .font(.system(size: 80, weight: .black))
+
+                    Spacer()
                 }
-
-                Image(systemName: "laurel.trailing")
-                    .font(.system(size: 80, weight: .black))
-
-                Spacer()
-            }
-            .foregroundStyle(
-                LinearGradient(
-                    colors: [
-                        .customGreenLight,
-                        .customGreenMedium,
-                        .customGreenDark,
-                    ],
-                    startPoint: .top,
-                    endPoint: .bottom
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [
+                            .customGreenLight,
+                            .customGreenMedium,
+                            .customGreenDark,
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
                 )
-            )
-            .padding(.top, 8)
+                .padding(.top, 8)
+
+                VStack(spacing: 8) {
+                    Text("Where can you find \nperfect tracks?")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+
+                    Text(
+                        "The hike which looks gorgeous in photos but is even better once you are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app."
+                    )
+                    .font(.footnote)
+                    .italic()
+
+                    Text("Dust off the boots! It's time for a walk.")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.customGreenMedium)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 16)
+                .frame(maxWidth: .infinity)
+            }  //: HEADER
+            .listRowSeparator(.hidden)
 
             // MARK: - SECTION: ICONS
 
-            VStack(spacing: 8) {
-                Text("Where can you find \nperfect tracks?")
-                    .font(.title2)
-                    .fontWeight(.heavy)
+            Section(header: Text("Alternate Icons")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        ForEach(alternateAppIcons.indices, id: \.self) { item in
+                            Button {
+                                print(
+                                    "Icon \(alternateAppIcons[item]) was pressed."
+                                )
+
+                                UIApplication.shared.setAlternateIconName(
+                                    alternateAppIcons[item]
+                                ) { error in
+                                    if error != nil {
+                                        print(
+                                            "Failed request to update the app's icon: \(String(describing: error?.localizedDescription))"
+                                        )
+                                    } else {
+                                        print(
+                                            "Success! You have changed the app's icon to \(alternateAppIcons[item])"
+                                        )
+                                    }
+                                }
+                            } label: {
+                                Image("\(alternateAppIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(16)
+                            }
+                            .buttonStyle(.borderless)
+                        }
+                    }
+                }  //: SCROLL VIEW
+                .padding(.top, 12)
 
                 Text(
-                    "The hike which looks gorgeous in photos but is even better once you are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app."
+                    "Choose your favourite app icon from the collection above."
                 )
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
                 .font(.footnote)
-                .italic()
+                .padding(.bottom, 12)
+            }  //: SECTION
+            .listRowSeparator(.hidden)
 
-                Text("Dust off the boots! It's time for a walk.")
-                    .fontWeight(.heavy)
-                    .foregroundColor(.customGreenMedium)
-            }
-            .multilineTextAlignment(.center)
-            .padding(.bottom, 16)
-            .frame(maxWidth: .infinity)
             // MARK: - SECTION: ABOUT
+
+            Section(
+                header: Text("ABOUT THE APP"),
+                footer: HStack {
+                    Spacer()
+                    Text("Copyright © All right reserved.")
+                    Spacer()
+                }
+                .padding(.vertical, 8)
+            ) {
+                // 1. Basic Labeled Content
+                // LabeledContent("Application", value: "Hike")
+
+                // 2. Advanced Labeled Content
+
+                CustomListRowView(
+                    rowLabel: "Application", rowIcon: "apps.iphone",
+                    rowContent: "HIKE", rowTintColor: .blue)
+
+                CustomListRowView(
+                    rowLabel: "Compatibility", rowIcon: "info.circle",
+                    rowContent: "iOS, iPadOS", rowTintColor: .red)
+
+                CustomListRowView(
+                    rowLabel: "Technology", rowIcon: "swift",
+                    rowContent: "Swift", rowTintColor: .orange)
+
+                CustomListRowView(
+                    rowLabel: "Version", rowIcon: "gear", rowContent: "1.0",
+                    rowTintColor: .purple)
+
+                CustomListRowView(
+                    rowLabel: "Developer", rowIcon: "ellipsis.curlybraces",
+                    rowContent: "Md.Hamid Hosen", rowTintColor: .mint)
+
+                CustomListRowView(
+                    rowLabel: "Designer", rowIcon: "paintpalette",
+                    rowContent: "Md.Hamid Hosen", rowTintColor: .pink)
+
+                CustomListRowView(
+                    rowLabel: "Website", rowIcon: "globe",
+                    rowTintColor: .indigo, rowLinkLabel: "Md.Hamid Hosen",
+                    rowLinkDestination: "https://sites.google.com/view/mdhamidhosen")
+
+            }  //: SECTION
         }  //: LIST
     }
 }
 
-#Preview {
-    SettingsView()
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
 }
